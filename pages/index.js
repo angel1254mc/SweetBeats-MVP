97969afc3f579e05bc4768754c976f1d1b8de6c0
aplan42 <alexharvey125@gmail.com>
2023-03-21 13:10:08 -0400

    Updated home screen

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
 import Link from 'next/link'
 import React from 'react'
-import Image from "../public/logo.jpg"
+import Image from "next/image";
 
 const index = () => {
   return (
-    <div>
+    <div class="home-page"> 
+<Image src={'/logo.png'}
+            width="900"
+            height="275"
+            alt={'SweetBeats'}/>
+<h1><br></br>
 
+A project by Angel Lopez Pol, Jacob Mass and Alex Harvey</h1>
+<br></br>
+<h1>   <Link href="/program"> Get started</Link></h1>
 
-      <img  src={'../public/logo.jpg'}/>
-
-      <img  src={Image}/>
-
-      <Link href="/program">Get started</Link>
-
+              
     </div>
+    
   )
 }
 
