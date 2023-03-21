9873ed6f590b72055aab747818ad75c012ab8c75
Angel Lopez Pol <angellopezpol1254@gmail.com>
2023-03-21 11:17:36 -0400

    b-dnd installed, ui made, implementing dnd is left

---
+++ components/InstrumentRow.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { InstrumentsContext } from "../hooks/InstrumentContext";
+import styles from "../styles/Measure2.module.css";
+import InstrumentImage from "./InstrumentImage";
+const InstrumentRow = ({ droppableProvidedInfo, instrument, measureId }) => {
+  const { instruments } = useContext(InstrumentsContext);
+  // Grab the instruments from the instrument context, check if the instruments matching 'instrument' exist in the current row
+  // if they do exist, render them in their correct spot
+  // Otherwise just dont render them
+  return (
+    <div
+      ref={droppableProvidedInfo.innerRef}
+      {...droppableProvidedInfo.droppableProps}
+      className={styles.row}
+    >
+      {
+        // Top ten worst practices I've engaged in for sure
+        [1, 2, 3, 4, 5].map((instrument_number) => {
+          if (instruments[instrument][instrument_number][measureId] != "ON") {
+            return <InstrumentImage key={instrument_number} empty={true}/>
+          } else return <InstrumentImage key={instrument_number} instrument={instrument} number={instrument_number} />;
+        })
+      }
+    </div>
+  );
+};
+
+export default InstrumentRow;
