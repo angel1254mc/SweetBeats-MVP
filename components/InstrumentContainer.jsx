485ed5b27ea095ca768e65e9251265cb26aed24c
Angel Lopez Pol <angellopezpol1254@gmail.com>
2023-03-28 11:11:22 -0400

    Here you go no more 5

--- components/InstrumentContainer.jsx
+++ components/InstrumentContainer.jsx
@@ -265,7 +265,16 @@ const InstrumentContainer = ({ name, color, instrument_ident }) => {
             ></button>
           </div>
         </div>
-        <div className={`${name} button-container-5`}>
+        
+      </div>
+    </div>
+  );
+};
+
+export default InstrumentContainer;
+
+/**
+ * <div className={`${name} button-container-5`}>
           <p>5</p>
           <div>
             <button
@@ -328,9 +337,4 @@ const InstrumentContainer = ({ name, color, instrument_ident }) => {
             ></button>
           </div>
         </div>
-      </div>
-    </div>
-  );
-};
-
-export default InstrumentContainer;
+ */
